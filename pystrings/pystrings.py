f74067149b5230226db5d77ed5c9814a0a2f2e03
mahdigadget <mahdigadget20@gmail.com>
2021-01-27 02:11:34 +0330

    first commit

---
+++ pystrings/pystrings.py
@@ -0,0 +1,7 @@
+class pystrings():
+    def __init__(self, *args):
+        for i in args:
+           for j in i:
+                j = str(j)
+                exec ('%s = %s' % (j, 'j'))
+                exec ('self.%s = %s' % (j, j))
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="pystrings",
+    version="0.0.1",
+    author="Mahdi Gadget",
+    author_email="mahdigadget20@gmail.com",
+    description="A package For ease of working with strings",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pystrings",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
