31fc40d417602fb7112eff923997e666339460cf
mahdigadget <mahdigadget20@gmail.com>
2021-01-29 19:18:41 +0330

    fix install bug

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ You can use pystrings as below:
 ```
 from pystrings import create_strings
 
-#create initialization 
-my_str = pystrings(['hell_world', 'use_it_for_handle_all_strings', 'haha'])
+#initialization 
+my_str = create_strings(['hell_world', 'use_it_for_handle_all_strings', 'haha'])
 
 #example for test it 
 print(my_str.haha) 
@@ -20,6 +20,8 @@ pystrings is an **open-source** library
 you can use it in all source code instead strings, like use in instead of ML hyperparameters 
 
 just you must inshialize pystrings with your strings and call it later 
+
+In the **Jupyter** web application this is a great function, when you select a cell for initialization and whenever you need to add a string, you update this cell and run it again.
 
 
 We have good news in the next updates
--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = []
-
 setup(
     name="pystrings",
-    version="0.0.32",
+    version="0.0.34",
     author="Mahdi Gadget",
     author_email="mahdigadget20@gmail.com",
     description="A package For ease of working with strings",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/pystrings",
-    packages=find_packages(),
-    install_requires=requirements,
+    URL = 'https://github.com/pystrings/pystrings',
+    packages=find_packages(include=['pystrings']),
+    python_requires = '>=3.6.0',
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
 )
